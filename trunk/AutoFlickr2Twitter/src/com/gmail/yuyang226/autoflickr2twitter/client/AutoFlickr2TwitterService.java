2adaa3abe07d9ae52dee0cf74090e565769b000d
yuyang226 <yuyang226@a020091c-255e-6d73-834a-340e5720189d>
2010-09-28 08:19:53 +0000

    we need http get

--- trunk/AutoFlickr2Twitter/src/com/gmail/yuyang226/autoflickr2twitter/client/AutoFlickr2TwitterService.java
+++ trunk/AutoFlickr2Twitter/src/com/gmail/yuyang226/autoflickr2twitter/client/AutoFlickr2TwitterService.java
@@ -4,11 +4,13 @@
 package com.gmail.yuyang226.autoflickr2twitter.client;
 
 import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
  * @author yayu
  *
  */
+@RemoteServiceRelativePath("recheck")
 public interface AutoFlickr2TwitterService extends RemoteService {
 
 	public void recheck();
