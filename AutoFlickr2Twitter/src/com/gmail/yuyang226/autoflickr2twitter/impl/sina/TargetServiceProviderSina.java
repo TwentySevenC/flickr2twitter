2c0435b6baf1ef7a481454f5945efcdc40895f5d
charleszq <charleszq@a020091c-255e-6d73-834a-340e5720189d>
2010-10-11 08:22:10 +0000

    update the sina login pages

--- AutoFlickr2Twitter/src/com/gmail/yuyang226/autoflickr2twitter/impl/sina/TargetServiceProviderSina.java
+++ AutoFlickr2Twitter/src/com/gmail/yuyang226/autoflickr2twitter/impl/sina/TargetServiceProviderSina.java
@@ -8,12 +8,10 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 import twitter4j.GeoLocation;
-import twitter4j.Status;
-import twitter4j.TwitterException;
 
+import com.gmail.yuyang226.autoflickr2twitter.datastore.MyPersistenceManagerFactory;
 import com.gmail.yuyang226.autoflickr2twitter.datastore.model.GlobalTargetApplicationService;
 import com.gmail.yuyang226.autoflickr2twitter.datastore.model.UserTargetService;
-import com.gmail.yuyang226.autoflickr2twitter.impl.twitter.TwitterPoster;
 import com.gmail.yuyang226.autoflickr2twitter.intf.ITargetServiceProvider;
 import com.gmail.yuyang226.autoflickr2twitter.model.IGeoItem;
 import com.gmail.yuyang226.autoflickr2twitter.model.IItem;
@@ -23,12 +21,13 @@ import com.gmail.yuyang226.autoflickr2twitter.sina.weibo4j.Weibo;
 
 /**
  * @author Toby Yu(yuyang226@gmail.com)
- *
+ * 
  */
 public class TargetServiceProviderSina implements ITargetServiceProvider {
 	public static final String ID = "sina";
-	private static final Logger log = Logger.getLogger(TwitterPoster.class.getName());
-	
+	private static final Logger log = Logger
+			.getLogger(TargetServiceProviderSina.class.getName());
+
 	/**
 	 * 
 	 */
@@ -36,79 +35,128 @@ public class TargetServiceProviderSina implements ITargetServiceProvider {
 		super();
 	}
 
-	/* (non-Javadoc)
-	 * @see com.gmail.yuyang226.autoflickr2twitter.intf.ITargetServiceProvider#getId()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.gmail.yuyang226.autoflickr2twitter.intf.ITargetServiceProvider#getId
+	 * ()
 	 */
 	@Override
 	public String getId() {
 		return ID;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.gmail.yuyang226.autoflickr2twitter.intf.ITargetServiceProvider#postUpdate(com.gmail.yuyang226.autoflickr2twitter.datastore.model.GlobalTargetApplicationService, com.gmail.yuyang226.autoflickr2twitter.datastore.model.UserTargetService, java.util.List)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.gmail.yuyang226.autoflickr2twitter.intf.ITargetServiceProvider#postUpdate
+	 * (com.gmail.yuyang226.autoflickr2twitter.datastore.model.
+	 * GlobalTargetApplicationService,
+	 * com.gmail.yuyang226.autoflickr2twitter.datastore.model.UserTargetService,
+	 * java.util.List)
 	 */
 	@Override
 	public void postUpdate(GlobalTargetApplicationService globalAppConfig,
 			UserTargetService targetConfig, List<IItem> items) throws Exception {
-		//api key and secret
+		// api key and secret
 		System.setProperty("weibo4j.oauth.consumerKey", Weibo.CONSUMER_KEY);
 		System.setProperty("weibo4j.oauth.consumerSecret",
 				Weibo.CONSUMER_SECRET);
 
 		Weibo weibo = new Weibo();
-		
-		//This is my own sina account, use this will post thing to my sina weibo
-		weibo.setToken("c9e5472c8723cc478a6332ce8b321008",
-				"5c206226f6cd6d87c75c1c60c601d27b");
+
+		// This is my own sina account, use this will post thing to my sina
+		// weibo
+		// weibo.setToken("c9e5472c8723cc478a6332ce8b321008",
+		// "5c206226f6cd6d87c75c1c60c601d27b");
+
+		weibo.setToken(targetConfig.getServiceAccessToken(), targetConfig
+				.getServiceTokenSecret());
 		for (IItem item : items) {
-	    	log.info("Posting message -> " + item + " for " + targetConfig.getServiceUserName());
+			log.info("Posting message -> " + item + " for "
+					+ targetConfig.getServiceUserName());
 
-	    	GeoLocation geoLoc = null;
-	    	if (item instanceof IGeoItem) {
-	    		if (((IGeoItem)item).getGeoData() != null) {
-	    			IGeoItem geoItem = (IGeoItem)item;
-	    			geoLoc = new GeoLocation(geoItem.getGeoData().getLatitude(), 
-	    					geoItem.getGeoData().getLongitude());
-	    		}
-	    	}
-	    	String message = null;
-	    	if (item instanceof IPhoto) {
-	    		IPhoto photo = (IPhoto)item;
-	    		message = photo.getTitle();
-	    		if (photo instanceof IShortUrl) {
-	    			message += " " + ((IShortUrl)photo).getShortUrl();
-	    		} else {
-	    			message += " " + photo.getUrl();
-	    		}
+			GeoLocation geoLoc = null;
+			if (item instanceof IGeoItem) {
+				if (((IGeoItem) item).getGeoData() != null) {
+					IGeoItem geoItem = (IGeoItem) item;
+					geoLoc = new GeoLocation(
+							geoItem.getGeoData().getLatitude(), geoItem
+									.getGeoData().getLongitude());
+				}
+			}
+			String message = null;
+			if (item instanceof IPhoto) {
+				IPhoto photo = (IPhoto) item;
+				message = photo.getTitle();
+				if (photo instanceof IShortUrl) {
+					message += " " + ((IShortUrl) photo).getShortUrl();
+				} else {
+					message += " " + photo.getUrl();
+				}
 
-	    	}
-	    	if (message != null) {
-	    		try {
-	    			if (geoLoc != null) {
-	    				weibo.updateStatus(message, geoLoc.getLatitude(), geoLoc.getLongitude());
-	    			} else {
-	    				weibo.updateStatus(message);
-	    			}
-	    		} catch (Exception e) {
-	    			log.warning("Failed posting message ->" + message + ". Cause: " + e);
-	    		}
-	    	}
-	    }
+			}
+			if (message != null) {
+				try {
+					if (geoLoc != null) {
+						weibo.updateStatus(message, geoLoc.getLatitude(),
+								geoLoc.getLongitude());
+					} else {
+						weibo.updateStatus(message);
+					}
+				} catch (Exception e) {
+					log.warning("Failed posting message ->" + message
+							+ ". Cause: " + e);
+				}
+			}
+		}
 		/**/
 	}
 
-	/* (non-Javadoc)
-	 * @see com.gmail.yuyang226.autoflickr2twitter.intf.IServiceAuthorizer#readyAuthorization(java.lang.String, java.util.Map)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seecom.gmail.yuyang226.autoflickr2twitter.intf.IServiceAuthorizer#
+	 * readyAuthorization(java.lang.String, java.util.Map)
 	 */
 	@Override
 	public String readyAuthorization(String userEmail, Map<String, Object> data)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (data == null || data.containsKey("token") == false) {
+			throw new IllegalArgumentException("Invalid data: " + data);
+		}
+		StringBuffer buf = new StringBuffer();
+
+		String token = String.valueOf(data.get("token"));
+		String secret = String.valueOf(data.get("secret"));
+
+		for (UserTargetService service : MyPersistenceManagerFactory
+				.getUserTargetServices(userEmail)) {
+			if (ID.equals(service.getTargetServiceProviderId())) {
+				throw new IllegalArgumentException(
+						"Target already registered: " + ID);
+			}
+		}
+		UserTargetService service = new UserTargetService();
+		service.setTargetServiceProviderId(ID);
+		service.setServiceAccessToken(token);
+		service.setServiceTokenSecret(secret);
+		service.setServiceUserId(userEmail);
+		service.setUserEmail(userEmail);
+		service.setServiceUserName(userEmail);
+		MyPersistenceManagerFactory.addTargetServiceApp(userEmail, service);
+
+		buf.append(ID).append(token).append(secret);
+		return buf.toString();
 	}
 
-	/* (non-Javadoc)
-	 * @see com.gmail.yuyang226.autoflickr2twitter.intf.IServiceAuthorizer#requestAuthorization()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seecom.gmail.yuyang226.autoflickr2twitter.intf.IServiceAuthorizer#
+	 * requestAuthorization()
 	 */
 	@Override
 	public Map<String, Object> requestAuthorization() throws Exception {
---
+++ AutoFlickr2Twitter/src/com/gmail/yuyang226/autoflickr2twitter/sina/weibo4j/http/SinaSignInCallBackServlet.java
@@ -0,0 +1,86 @@
+package com.gmail.yuyang226.autoflickr2twitter.sina.weibo4j.http;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gmail.yuyang226.autoflickr2twitter.impl.sina.TargetServiceProviderSina;
+import com.gmail.yuyang226.autoflickr2twitter.sina.weibo4j.Weibo;
+import com.gmail.yuyang226.autoflickr2twitter.sina.weibo4j.WeiboException;
+
+public class SinaSignInCallBackServlet extends HttpServlet {
+
+	private static final Logger log = Logger
+			.getLogger(SinaSignInCallBackServlet.class.getName());
+	private static final long serialVersionUID = -1016731113426658126L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		// Get the verifier;
+		String verifier = req.getParameter("verifier");
+		log.log(Level.INFO, "oauth_verifier = " + verifier);
+
+		// Request token and secret
+		String requestToken = req.getParameter("token");
+		String requestTokenSecret = req.getParameter("secret");
+
+		if (verifier == null) {
+			resp.getWriter().println("Error to get the verifier.");
+			return;
+		}
+
+		if (requestTokenSecret == null || requestToken == null) {
+			resp.getWriter().println("Error to get request token from cookie.");
+			return;
+		}
+
+		System.setProperty("weibo4j.oauth.consumerKey", Weibo.CONSUMER_KEY);
+		System.setProperty("weibo4j.oauth.consumerSecret",
+				Weibo.CONSUMER_SECRET);
+
+		Weibo weibo = new Weibo();
+		try {
+			AccessToken accessToken = weibo.getOAuthAccessToken(requestToken, requestTokenSecret,
+					verifier);
+			if (accessToken == null) {
+				resp.getWriter().println("Error to get access token.");
+				return;
+			} else {
+				resp.getWriter().println("Authorization done.");
+				// TODO save the token into db.
+				TargetServiceProviderSina tps = new TargetServiceProviderSina();
+				Map<String,Object> data = new HashMap<String,Object>();
+				data.put("token",accessToken.getToken());
+				data.put("secret", accessToken.getTokenSecret());
+				try {
+					tps.readyAuthorization("charleszq@gmail.com", data);
+				} catch (Exception e) {
+					resp.getWriter().println("<p>But error to save the access token.");
+					log.log(Level.WARNING, e.getMessage());
+				}
+				return;
+			}
+		} catch (WeiboException e) {
+			resp.getWriter().println("Error: " + e.getMessage());
+		}
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req,resp);
+	}
+	
+	
+
+}
