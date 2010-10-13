00d43e4848c37e1fc15d35b7d89d5bad8a980bce
deepnighttwo <deepnighttwo@a020091c-255e-6d73-834a-340e5720189d>
2010-10-13 07:03:52 +0000

    Dynamic create source and target page content.

--- AutoFlickr2Twitter/src/com/gmail/yuyang226/autoflickr2twitter/intf/IServiceProvider.java
+++ AutoFlickr2Twitter/src/com/gmail/yuyang226/autoflickr2twitter/intf/IServiceProvider.java
@@ -7,16 +7,17 @@ import com.gmail.yuyang226.autoflickr2twitter.datastore.model.GlobalApplicationC
 
 /**
  * @author Toby Yu(yuyang226@gmail.com)
- *
+ * 
  */
 public interface IServiceProvider<T extends GlobalApplicationConfig> {
-	
+
 	/**
 	 * A lower case representation of the source service provider.
+	 * 
 	 * @return a string to represent the underlying source service provider.
 	 */
 	public String getId();
-	
+
 	public T createDefaultGlobalApplicationConfig();
 
 }
