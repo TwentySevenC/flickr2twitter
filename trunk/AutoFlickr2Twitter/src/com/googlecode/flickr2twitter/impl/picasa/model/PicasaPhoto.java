f9ee9f632259c73d048f4340c6d08894952a5cd7
yuyang226 <yuyang226@a020091c-255e-6d73-834a-340e5720189d>
2010-10-19 14:39:21 +0000

    support for picasa web album

---
+++ trunk/AutoFlickr2Twitter/src/com/googlecode/flickr2twitter/impl/picasa/model/PicasaPhoto.java
@@ -0,0 +1,131 @@
+/**
+ * 
+ */
+package com.googlecode.flickr2twitter.impl.picasa.model;
+
+import java.util.Date;
+
+import com.googlecode.flickr2twitter.model.IPhoto;
+
+/**
+ * @author Toby Yu(yuyang226@gmail.com)
+ *
+ */
+public class PicasaPhoto implements IPhoto {
+
+	/**
+	 * 
+	 */
+	public PicasaPhoto() {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IPhoto#getDatePosted()
+	 */
+	@Override
+	public Date getDatePosted() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IPhoto#getDateTaken()
+	 */
+	@Override
+	public Date getDateTaken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IPhoto#getUrl()
+	 */
+	@Override
+	public String getUrl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IPhoto#setDatePosted(java.util.Date)
+	 */
+	@Override
+	public void setDatePosted(Date datePosted) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IPhoto#setDateTaken(java.util.Date)
+	 */
+	@Override
+	public void setDateTaken(Date dateTaken) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IPhoto#setUrl(java.lang.String)
+	 */
+	@Override
+	public void setUrl(String url) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IItem#getDescription()
+	 */
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IItem#getId()
+	 */
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IItem#getTitle()
+	 */
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IItem#setDescription(java.lang.String)
+	 */
+	@Override
+	public void setDescription(String description) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IItem#setId(java.lang.String)
+	 */
+	@Override
+	public void setId(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.googlecode.flickr2twitter.model.IItem#setTitle(java.lang.String)
+	 */
+	@Override
+	public void setTitle(String title) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
