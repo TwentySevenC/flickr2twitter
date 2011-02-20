6c6f1dab5a17cdeb312dc8a96d065d5feacc7498
deepnighttwo <deepnighttwo@a020091c-255e-6d73-834a-340e5720189d>
2011-02-20 04:37:48 +0000

    show ebay user profile before adding to database

--- branches/ebaysocialhub/AutoFlickr2Twitter/src/com/googlecode/flickr2twitter/servlet/EbayConfigServlet.java
+++ branches/ebaysocialhub/AutoFlickr2Twitter/src/com/googlecode/flickr2twitter/servlet/EbayConfigServlet.java
@@ -11,56 +11,76 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.xml.sax.SAXException;
+
 import com.googlecode.flickr2twitter.datastore.MyPersistenceManagerFactory;
 import com.googlecode.flickr2twitter.datastore.model.User;
 import com.googlecode.flickr2twitter.datastore.model.UserSourceServiceConfig;
+import com.googlecode.flickr2twitter.impl.ebay.EbayUser;
+import com.googlecode.flickr2twitter.impl.ebay.GetUserProfileDAO;
 import com.googlecode.flickr2twitter.impl.ebay.SourceServiceProviderEbay;
 
 /**
  * @author Emac Shen (shen.bin.1983@gmail.com)
  */
-public class EbayConfigServlet extends HttpServlet
-{
+public class EbayConfigServlet extends HttpServlet {
 
-    private static final long  serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public static final String PARA_SELLER_ID   = "seller_id";
+	public static final String PARA_SELLER_ID = "seller_id";
 
-    /*
-     * (non-Javadoc)
-     * @see javax.servlet.http.HttpServlet#service(javax.servlet.http.HttpServletRequest ,
-     * javax.servlet.http.HttpServletResponse)
-     */
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException
-    {
-        User user = (User) req.getSession().getAttribute(UserAccountServlet.PARA_SESSION_USER);
+	public static final String PARA_SEARCH_SELLER_ID = "search_seller_id";
 
-        if ( user == null )
-        {
-            req.getSession().setAttribute("message", "Please Login first!");
-            resp.sendRedirect("/index.jsp");
-            return;
-        }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.servlet.http.HttpServlet#service(javax.servlet.http.HttpServletRequest
+	 * , javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		User user = (User) req.getSession().getAttribute(
+				UserAccountServlet.PARA_SESSION_USER);
 
-        String userEmail = user.getUserId().getEmail();
-        String sellerId = req.getParameter(PARA_SELLER_ID);
+		if (user == null) {
+			req.getSession().setAttribute("message", "Please Login first!");
+			resp.sendRedirect("/index.jsp");
+			return;
+		}
 
-        // TODO#EMAC.P! check seller's identity
-        
-        UserSourceServiceConfig serviceConfig = new UserSourceServiceConfig();
-        // TODO#EMAC.P1 store seller id in user id temporarily
-        serviceConfig.setServiceUserId(sellerId);
-        // TODO#EMAC.P? is it possible to get seller name?
-        serviceConfig.setServiceUserName(sellerId);
-        serviceConfig.setServiceProviderId(SourceServiceProviderEbay.ID);
-        serviceConfig.setUserEmail(userEmail);
-        // TODO#EMAC.P? is it possible to get seller's url?
-        serviceConfig.setUserSiteUrl("");
-        MyPersistenceManagerFactory.addSourceServiceApp(userEmail, serviceConfig);
+		String userEmail = user.getUserId().getEmail();
+		String sellerId = req.getParameter(PARA_SELLER_ID);
 
-        resp.sendRedirect("/authorize.jsp");
-    }
+		GetUserProfileDAO getUserProfileDAO = new GetUserProfileDAO();
+
+		EbayUser ebayUser = null;
+		try {
+			ebayUser = getUserProfileDAO.getUserProfile(true, sellerId);
+		} catch (SAXException e) {
+			throw new ServletException("Unable to found user profile for id: "
+					+ sellerId, e);
+		}
+
+		UserSourceServiceConfig serviceConfig = new UserSourceServiceConfig();
+		// TODO#EMAC.P1 store seller id in user id temporarily
+		serviceConfig.setServiceUserId(sellerId);
+
+		String userDisplayName = sellerId;
+		String storeName = ebayUser.getStoreName();
+		if ((storeName != null) && (storeName.length() > 0)) {
+			userDisplayName += " (" + storeName + ")";
+		}
+		serviceConfig.setServiceUserName(userDisplayName);
+		serviceConfig.setServiceProviderId(SourceServiceProviderEbay.ID);
+		serviceConfig.setUserEmail(userEmail);
+
+		serviceConfig.setUserSiteUrl(ebayUser.getStoreURL());
+		MyPersistenceManagerFactory.addSourceServiceApp(userEmail,
+				serviceConfig);
+
+		resp.sendRedirect("/authorize.jsp");
+	}
 
 }
